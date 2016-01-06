9f0f69628f922d933628b97762aac099b5febbd2
yudhi.k.surtan <yudhi.k.surtan@gdn-commerce.com>
2016-01-07 00:57:39 +0700

    add new configuration and todo

--- wish-to-japan-view/src/main/java/org/jug/brainmaster/ws/startup/ApplicationConfig.java
+++ wish-to-japan-view/src/main/java/org/jug/brainmaster/ws/startup/ApplicationConfig.java
@@ -1,10 +1,5 @@
 package org.jug.brainmaster.ws.startup;
 
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.File;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
@@ -13,13 +8,20 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+//TODO apaan lagi ini :( stress ya si willy
 @ManagedBean(eager = true)
 @ApplicationScoped
 @Named
 public class ApplicationConfig {
-  private Properties properties;
   private static final String JBOSS_CONFIG_DIRECTORY_KEY = "jboss.server.config.dir";
   private static final String APPLICATION_CONFIG_FILENAME = "wish-to-japan-config.properties";
+  private Properties properties;
 
   @Inject
   private Logger log;
@@ -31,22 +33,8 @@ public class ApplicationConfig {
   private final String VIEW_HOST = "wish.to.japan.view.host.url";
   private final String LOGIC_HOST = "wish.to.japan.logic.host.url";
 
-  public void setProperties(Properties properties) {
-    this.properties = properties;
-  }
-
-  @PostConstruct
-  private void postConstruct () {
-    try {
-      properties = new Properties();
-      FileReader reader = new FileReader(
-          System.getProperty(JBOSS_CONFIG_DIRECTORY_KEY) + File.separator
-              + APPLICATION_CONFIG_FILENAME);
-      properties.load(reader);
-      log.log(Level.SEVERE, "" + properties.size());
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+  public String getLogicHost() {
+    return properties.getProperty(LOGIC_HOST);
   }
 
   public Date getStartDate() {
@@ -60,19 +48,33 @@ public class ApplicationConfig {
     return startDate;
   }
 
-  public int getWinnerTimeout() {
-    return Integer.parseInt(properties.getProperty(WINNER_TIMEOUT));
-  }
-
-  public boolean useWss() {
-    return Boolean.parseBoolean(properties.getProperty(USE_WSS));
-  }
-
   public String getViewHost() {
     return properties.getProperty(VIEW_HOST);
   }
 
-  public String getLogicHost() {
-    return properties.getProperty(LOGIC_HOST);
+  public int getWinnerTimeout() {
+    return Integer.parseInt(properties.getProperty(WINNER_TIMEOUT));
+  }
+
+  @PostConstruct
+  private void postConstruct () {
+    try {
+      properties = new Properties();
+      FileReader reader = new FileReader(
+          System.getProperty(JBOSS_CONFIG_DIRECTORY_KEY) + File.separator
+          + APPLICATION_CONFIG_FILENAME);
+      properties.load(reader);
+      log.log(Level.SEVERE, "" + properties.size());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void setProperties(Properties properties) {
+    this.properties = properties;
+  }
+
+  public boolean useWss() {
+    return Boolean.parseBoolean(properties.getProperty(USE_WSS));
   }
 }
